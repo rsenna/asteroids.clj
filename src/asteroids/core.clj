8ddb64383a76cd0ea2b441aa81558f5e1dd67374
Rogério Senna [Mac] <rsenna@live.com>
2015-09-17 23:37:01 -0300

    collision

--- src/asteroids/core.clj
+++ src/asteroids/core.clj
@@ -5,3 +5,22 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;; TODO: refactor
+(defn radius [x]
+  (if (number? (:radius x))
+    (:radius x)
+    ((:radius x))))
+
+;; TODO: refactor
+(defn position [x]
+  (if (sequential? (:position x))
+    (:position x)
+    ((:position x))))
+
+(defn intersects? [a b]
+  (let [min-dist (+ (radius a) (radius b))
+        dist     (map - (position a) (position b))]
+    (> (* min-dist min-dist) (length-squared dist))))
+
+
